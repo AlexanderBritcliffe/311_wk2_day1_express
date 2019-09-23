b483b8989db029ffd05cef172d2a32ce3c63fa86
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2019-09-23 10:45:11 -0500

    still need to post and alter routes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const express = require('express')
-const body = require('body-parser')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
-
 const { users } = require('./state')
+app.use(bodyParser.json());
 
 /* BEGIN - create routes here */
 app.get("/users",(req,res)=> {
